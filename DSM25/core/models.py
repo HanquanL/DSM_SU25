e312af1e6a09e1932b5b32db7825ba9243b24d1d
HanquanL <Hanquanl0115@gmail.com>
2025-08-10 16:14:00 -0400

    workin gon core

--- DSM25/core/models.py
+++ DSM25/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    Cust_ID = models.AutoField(primary_key=True)
+    CustFirstName = models.CharField(max_length=50)
+    CustLastName = models.CharField(max_length=50)
+    CustMiddleInit = models.CharField(max_length=1)
+    CustSuffix = models.CharField(max_length=10)
+    CustDOB = models.DateField()
+    Gender = models.CharField(max_length=10)
+    CustomerType = models.CharField(max_length=50)
+
