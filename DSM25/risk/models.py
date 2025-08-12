c7b6b53770f428f61dc61e21766e3ffdb470a4cf
HanquanL <Hanquanl0115@gmail.com>
2025-08-12 15:13:41 -0400

    redo risk pipeline

--- DSM25/risk/models.py
+++ DSM25/risk/models.py
@@ -3,8 +3,3 @@ from core.models import Customer
 
 
 # Create your models here.
-class Risk_score(models.Model):
-    Patient_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    Risk_level = models.CharField(max_length=50)
-    Score = models.FloatField()
-    
